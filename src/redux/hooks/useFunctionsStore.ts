7a52476527b1f2854e92c2e8bea3cd398a217a08
Héctor Pimentel <pimenteldeveloper@gmail.com>
2024-07-23 02:12:34 -0400

    refactor function in orderslice

--- src/redux/hooks/useFunctionsStore.ts
+++ src/redux/hooks/useFunctionsStore.ts
@@ -1,35 +1,27 @@
-export function UpdateNewListItems(newListItems, orders, list, productCount) {
-  orders.forEach(function (product) {
-    var countItemsInProduct = product?.product_items?.length || 0
-    for (var i = 0; i < countItemsInProduct; i++) {
-      newListItems.forEach(function (li, index) {
-        if (li?.item_id === product?.product_items?.[i]?.item_id) {
-          newListItems[index] = {
-            ...li,
-            item_count:
-              li.item_count -
-              (product?.product_items?.[i]?.item_count || 0) * productCount,
+export function updateNewListItems(newListItems, orders, list, productCount) {
+  // Función para actualizar los elementos de la lista
+  function updateListItems(products) {
+    products.forEach((product) => {
+      product?.product_items?.forEach((productItem) => {
+        newListItems.forEach((listItem, index) => {
+          if (listItem?.item_id === productItem?.item_id) {
+            newListItems[index] = {
+              ...listItem,
+              item_count:
+                listItem.item_count -
+                (productItem?.item_count || 0) * productCount,
+            }
           }
-        }
+        })
       })
-    }
-  })
+    })
+  }
 
-  list.forEach(function (product) {
-    var countItemsInProduct = product?.product_items?.length || 0
-    for (var i = 0; i < countItemsInProduct; i++) {
-      newListItems.forEach(function (li, index) {
-        if (li?.item_id === product?.product_items?.[i]?.item_id) {
-          newListItems[index] = {
-            ...li,
-            item_count:
-              li.item_count -
-              (product?.product_items?.[i]?.item_count || 0) * productCount,
-          }
-        }
-      })
-    }
-  })
+  // Actualizar newListItems con los pedidos
+  updateListItems(orders)
+
+  // Actualizar newListItems con la lista
+  updateListItems(list)
 
   return newListItems
 }
